bb4e921ef7a8b7d8d47aef3afdafad985e673cfb
pfang12 <101620013+pfang12@users.noreply.github.com>
2024-03-12 14:21:46 -0700

    Update Navbar.jsx

--- App/src/components/Navbar.jsx
+++ App/src/components/Navbar.jsx
@@ -5,7 +5,6 @@ function Navbar() {
   return (
     <nav className="navbar">
       <img src={logo} alt="Logo" style={{ width: '300px', height: '100px' }} className="navbar-logo" />
-      <h1 className="navbar-title">Navbar</h1>
     </nav>
   );
 }
